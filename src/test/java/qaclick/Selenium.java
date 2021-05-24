b92aa7f4c6a62e68f1748b202d943ce466b8e5ec
demoq <raufsqa@gmail.com>
2021-05-24 16:23:31 -0700

    first commit

---
+++ src/test/java/qaclick/Selenium.java
@@ -0,0 +1,20 @@
+package qaclick;
+
+import org.testng.annotations.Test;
+
+public class Selenium {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+	
+
+}
